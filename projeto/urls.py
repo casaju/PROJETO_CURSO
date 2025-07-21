7e4fbb1c30fb0b6a032c6c4d7a32ca7bd66cc1c5
JULIA AMBROSINI <juliacambrosini@gmail.com>
2025-07-20 23:19:35 -0300

    HttpResponse

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 def my_view(request):
-    ...
+    return HttpResponse("HIII, is working!")
 
 
 urlpatterns = [
