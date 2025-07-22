95b0de3b7b6a6ce35d9563d1a5da79e931c5d9a4
JULIA AMBROSINI <juliacambrosini@gmail.com>
2025-07-21 23:33:33 -0300

    Removed Contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html')
 
-def about(request):
-    return HttpResponse("ABOUT PAGE IS WORKING!")
-
-def contact(request):
-    return render(request, 'recipes/contact.html')
 
