0a1cd7d0f0e7f16890dd3c7cd750bd75fff6df1b
JULIA AMBROSINI <juliacambrosini@gmail.com>
2025-07-23 01:09:35 -0300

    app name update

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from recipes import views
 
-app_name = 'recipes'
+app_name = 'recipes' # Namespace for the app
+
 urlpatterns = [
-    path('', views.home, name='recipes:home'), 
-    path('recipes/<int:id>/', views.recipe, name='recipe' ),
+    path('', views.home, name='recipes-home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe-recipe' ),
 ]
