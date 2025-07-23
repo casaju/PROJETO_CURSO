aabe67b200b68e5684880a9b3bacd2273234f7da
JULIA AMBROSINI <juliacambrosini@gmail.com>
2025-07-22 23:02:02 -0300

    putting datas in view.home

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context= {'name': 'Júlia Casagrande'})
+    return render(request, 'recipes/pages/home.html', context= {'recipes': [make_recipe() for _ in range(10)]})
 
 def recipes(request,id):
-    return render(request, 'recipes/pages/recipe-page.html', context= {'name': 'Júlia Casagrande'})
-
+    return render(request, 'recipes/pages/recipe-page.html', context= {'recipe': make_recipe()})
