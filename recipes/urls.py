caf1f1b7e63561654a7680aad71e0876a2d0a1f8
JULIA AMBROSINI <juliacambrosini@gmail.com>
2025-07-23 00:41:16 -0300

    urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.home ),
-    path('recipes/<int:id>/', views.recipes ),
+    path('', views.home, name='recipes-home'), 
+    path('recipes/<int:id>/', views.recipe, name='recipe-recipe' ),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context= {'recipes': [make_recipe() for _ in range(10)]})
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
-def recipes(request,id):
-    return render(request, 'recipes/pages/recipe-page.html', context= {'recipe': make_recipe()})
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-page.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
+# This view is for displaying a single recipe page.
+# It uses the make_recipe function to generate a recipe object
+
