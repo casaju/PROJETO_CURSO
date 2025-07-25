bfd9169bd6381477460d1c5ff0da9217ac2d0e9b
JULIA AMBROSINI <juliacambrosini@gmail.com>
2025-07-24 22:38:25 -0300

    Admin configs

--- projeto/settings.py
+++ projeto/settings.py
@@ -121,6 +121,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'static'  # Directory where static files will be collected
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # Directory where media files will be stored
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
