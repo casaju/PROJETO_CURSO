4dedf69fa5f7a42cdb5c3b378d5acefdb6f1b458
Julia <juliacambrosini@gmail.com>
2025-08-04 22:36:37 -0300

    preparation steps

--- recipes/views.py
+++ recipes/views.py
@@ -22,7 +22,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(id=id, is_published=True).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-page.html', context={
         'recipe': recipe,
         'is_detail_page': True,
